c84e651b28f367612503cad9342170ac16496b64
Yurii Solodzhuk <yurii.solodzhuk@devcom.com>
2023-02-03 18:57:01 +0200

    Added emoji

--- src/main/java/io/project/SpringDemoBot/service/TelegramBot.java
+++ src/main/java/io/project/SpringDemoBot/service/TelegramBot.java
@@ -1,5 +1,6 @@
 package io.project.SpringDemoBot.service;
 
+import com.vdurmont.emoji.EmojiParser;
 import io.project.SpringDemoBot.config.BotConfig;
 import io.project.SpringDemoBot.model.User;
 import io.project.SpringDemoBot.repository.UserRepository;
@@ -103,7 +104,8 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private void startCommandReceived(long chatId, String name) {
-        String answer = "Hi, " + name + ", nice to meet you!";
+        String answer = EmojiParser.parseToUnicode("Hi, " + name + ", nice to meet you!" + " :blush:");
+//        String answer = "Hi, " + name + ", nice to meet you!";
         log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
